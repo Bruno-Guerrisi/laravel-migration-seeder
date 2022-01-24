ecc568aa9acbfe772a0a9e73628da9d201e71c84
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2022-01-24 17:57:55 +0100

    push in travels

--- database/seeds/TravelsTableSeeder.php
+++ database/seeds/TravelsTableSeeder.php
@@ -22,7 +22,7 @@ class TravelsTableSeeder extends Seeder
 
             $new_travel->poster = $faker->imageUrl(640, 480, 'animals', true);
             $new_travel->title = $faker->text(20);
-            $new_travel->description = $faker->paragraphs(2, true);
+            $new_travel->description = $faker->paragraphs(1, true);
             $new_travel->city = $faker->word();
             $new_travel->price = rand(80, 500);
             $new_travel->max_people = rand(10, 50);
