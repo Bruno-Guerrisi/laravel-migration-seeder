eb62509f6b5f9e6b73906dee87b41dc7af265700
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2022-01-24 17:53:33 +0100

    first commit

---
+++ database/seeds/TravelsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+
+use App\Travel;
+
+class TravelsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+        for ($i=0; $i < 20; $i++) { 
+            
+            $new_travel = new Travel();
+
+            $new_travel->poster = $faker->imageUrl(640, 480, 'animals', true);
+            $new_travel->title = $faker->text(20);
+            $new_travel->description = $faker->paragraphs(2, true);
+            $new_travel->city = $faker->word();
+            $new_travel->price = rand(80, 500);
+            $new_travel->max_people = rand(10, 50);
+
+
+            $new_travel->save();
+
+        }
+
+    }
+}
